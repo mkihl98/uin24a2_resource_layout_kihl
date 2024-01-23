13e3d282adb7def364e609eff6fef2d4b7375646
mkihl98 <martinki@hiof.no>
2024-01-23 16:05:50 +0100

    Javascript og litt CSS
    Har lagt til javascript-kode (ferdig, tror jeg), og litt CSS.

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
+document.addEventListener("DOMContentLoaded", addResource('HTML'))
+
 console.log(resources)
 
-let resourcesHTML = ""
+let menuHTML = "<ul>"
 
-resources.map(resource => resourcesHTML += 
-    `<article>
+resources.map(resource => menuHTML += 
+    `<li onClick="addResource('${resource.category}')">${resource.category}</li>`
+)
+
+const nav = document.getElementsByTagName("nav")
+nav[0].innerHTML = `${menuHTML}</ul>`
+
+console.log(menuHTML)
+
+function addResource(title) {
+    let resourcesHTML = ""
+
+    let elements = resources.filter(resource => title === resource.category)
+
+    elements.map(element => { resourcesHTML +=
+        `<h2>${element.category}</h2>
+        <p>${element.text}</p>`
+
+        element.sources.map(e => resourcesHTML +=
+            `<ul>
+                <li><a href="${e.url}">${e.title}</a></li>
+            </ul>`
+        )
+    })
+
+    const main = document.getElementsByTagName("main")
+    main[0].innerHTML = resourcesHTML
+}
+
+
+/*`<article>
         <h2>${resource.category}</h2>
         <p>${resource.text}</p>
         <ul>
@@ -12,9 +43,4 @@ resources.map(resource => resourcesHTML +=
             <li><a href="${resource.url}">${resource.title}</a></li>
         </ul>
     </article>`
-)
-
-console.log(resourcesHTML)
-
-const main = document.getElementsByTagName("main")
-main[0].innerHTML = resourcesHTML
+*/
